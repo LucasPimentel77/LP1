c8b5313513fbfefac478cb940e82ef8759dee529
LucasPimentel77 <116539949+LucasPimentel77@users.noreply.github.com>
2024-06-12 09:55:45 -0400

    Add files via upload

---
+++ Ex_13_6.java
@@ -0,0 +1,15 @@
+package capitulo3;
+
+public class Ex_13_6 {
+
+	public static void main(String[] args) {
+		int i, a=0, b=1;
+		System.out.print("0, 1, ");
+		for(i=0; i<10; i++) {
+			a = a + b;
+			System.out.print(a + ", ");
+			
+		}
+	}
+
+}
