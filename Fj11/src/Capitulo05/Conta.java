da56cdb99f78b255f7896ce0561046fcc3425c7a
Lucas dos Santos Pimentel <3341864@ifmt.local>
2024-07-25 11:27:54 -0400

    atualizando capítulo 5

---
+++ Fj11/src/Capitulo05/Conta.java
@@ -0,0 +1,91 @@
+package Capitulo05;
+
+public class Conta {
+	private static int identificador;
+	private String titular;
+	private int numero;
+	private String agencia;
+	private double saldo;
+	private String data;
+	
+	
+	public Conta(String Titular) {
+		setTitular(Titular);
+		identificador = identificador + 1;
+	}
+	
+	public Conta() {
+		identificador = identificador + 1;
+	}
+
+	public void sacar(int valor){
+		if (getSaldo() >= valor){
+			setSaldo(getSaldo()-valor);
+			System.out.println("saque realizado");
+		}else {
+			System.out.println("saldo insuficiente");
+		}
+	}
+	
+	public void depositar(int valor) {
+		setSaldo(getSaldo()+valor);
+		System.out.println("deposito realixado");
+	}
+	
+
+	public double calculaRendimento() {
+		return this.saldo*0.1;
+	}
+
+
+	public String getTitular() {
+		return titular;
+	}
+
+	public void setTitular(String titular) {
+		this.titular = titular;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public String getAgencia() {
+		return agencia;
+	}
+
+	public void setAgencia(String agencia) {
+		this.agencia = agencia;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+	
+	public static int getIdentificador() {
+		return identificador;
+	}
+	
+	@Override
+	public String toString() {
+		return "Conta [nomeDoTitular=" + titular + ", numero=" + numero + ", agencia=" + agencia + ", saldo="
+				+ saldo + ", dataDeAbertura=" + data + "]";
+	}
+	
+}
