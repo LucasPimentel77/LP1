faf252d56d196bd301beca701e46570fb91ab24d
Lucas dos Santos Pimentel <3341864@ifmt.local>
2024-08-05 12:27:28 -0400

    implementando classe funcionario

---
+++ Fj11/src/br/com/caelum/contas/modelo/Gerente.java
@@ -0,0 +1,31 @@
+package br.com.caelum.contas.modelo;
+
+public class Gerente extends Funcionario{
+	private int senha;
+	private int numeroDeFuncionariosGerenciados;
+	
+	public boolean autentica(int senha) {
+		if(this.senha == senha) {
+			System.out.println("acesso permitido");
+			return true;
+		} else {
+			System.out.println("acesso negado");
+			return false;
+		}
+	}
+	
+	@Override
+	public double getBonificacao(){
+		
+	}
+
+	public int getSenha() {
+		return senha;
+	}
+
+	public void setSenha(int senha) {
+		this.senha = senha;
+	}
+	
+
+}
